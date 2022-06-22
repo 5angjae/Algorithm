437b3c79a0ff00ba58b424250b0783304bd26252
5angjae <sangjea5@naver.com>
2022-06-22 11:23:36 +0900

    BOJ1013 finish

--- BAEKJOON/BJ1013_new.py
+++ BAEKJOON/BJ1013_new.py
@@ -2,15 +2,20 @@
 # 어디서 indexError가 뜨는건지 확인할 필요가 있다.
 
 import sys
-input = sys.stdin.readline
+
 def pattern(text):
+    # print(text)
     if len(text) == 0:
         print("YES")
+        return
+    elif len(text) == 1: # text 길이가 1이 되면 01, 100 을 찾기 위해서 탐색할 때 인덱스 에러가 난다.
+        print("NO")
         return
     else:
         if text[0] == "0":
             if text[1] == "1":
                 pattern(text[2:])
+                return
             else:
                 print("NO")
                 return
@@ -32,9 +37,15 @@ def pattern(text):
                         elif text[idx] == "1" and state == 0:
                             change += 1
                             state = 1
+                        if change == 1 and text[idx+1:idx+4] == "100": # 1001/1001 연달아 나올 때 잘못 나눠줘서 해결하기 위한 코드
+                            break
                         idx += 1
+                    if change == 1 and text[idx+1:idx+4] =="100":
+                        pattern(text[idx+1:])
+                        return
                     if change == 2:
                         pattern(text[idx - 1:])
+                        return
                     if idx == len(text):
                         if change == 1:
                             print("YES")
@@ -48,7 +59,7 @@ def pattern(text):
 
 
 T = int(input())
-texts = [input().strip() for _ in range(T)]
+texts = [input() for _ in range(T)]
 for text in texts:
     if len(text) == 0:
         print("NO")
