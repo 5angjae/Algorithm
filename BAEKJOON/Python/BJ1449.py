a442715cab87c981fd51f880c4dfec399fdb9b38
5angjae <sangjea5@naver.com>
2022-06-29 20:48:54 +0900

    BOJ 1449 python

---
+++ BAEKJOON/Python/BJ1449.py
@@ -0,0 +1,22 @@
+# BOJ 1449 수리공 항승
+
+def tape(x):
+    for i in range(x, x+L):
+        #print(i)
+        cover.append(i)
+    #print("-----------------")
+
+N, L = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+cover = []
+cnt = 0
+
+for a in arr:
+    if a not in cover:
+        tape(a)
+        cnt += 1
+    else:
+        pass
+
+print(cnt)
